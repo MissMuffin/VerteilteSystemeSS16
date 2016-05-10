3abb75117a7e6c35b98cee393edad2e0f4189074
MissMuffin <fyyree@gmail.com>
2016-05-10 23:15:01 +0200

    added javadoc to abgabe uebung 2

--- Uebung/VerteilteSystemeUebung/src/util/Logger.java
+++ Uebung/VerteilteSystemeUebung/src/util/Logger.java
@@ -1,33 +1,57 @@
 package util;
 
-//TODO No documentation
+/** 
+ * Console logging class for info and error messages
+ * 
+ * @author Haase
+ */
+
 public class Logger {
 
+	/**
+	 * Static Logger instance
+	 */
 	private static Logger instance;
 
 
-	private Logger() {
-	}
+	/**
+	 * Private constructor
+	 */
+	private Logger() {}
 
+	/**
+	 * returns static Logger instance
+	 * if no instance exists yet the constructor will be called
+	 * @return Logger
+	 */
 	public static Logger getInstance() {
 		if (Logger.instance == null)
 			Logger.instance = new Logger();
 		return instance;
 	}
 
+	/**
+	 * Logs a info message
+	 * @param to_log the message to be logged
+	 */
 	public void info(String to_log) {
 		log("Info", to_log);
 	}
 
+	/**
+	 * Logs an error message
+	 * @param to_log the error message to be logged
+	 */
 	public void error(String to_log) {
 		log("Error", to_log);
 	}
 
+	/**
+	 * Prints a message and its level to the console as a new line
+	 * @param level		the level of the log: error or info
+	 * @param to_log	the message to be logged
+	 */
 	private void log(String level, String to_log) {
-		//Hier koennen Sie entscheiden an welchen Ort die Logs geschrieben werden sollen
-		//z.B. auf stdout, in eine Datei oder beides..
 		System.out.println(level + ": " + to_log);
-		//Bei Programmen die ueber eine laengeren Zeitraum laufen waeren Ausgaben im folgendem Format sicherlich sinnvoll:
-		//ERROR [07.10.2015-15:30:15] [SeverExample.java] Socket creation failed! Address already in use.
 	}
 }
