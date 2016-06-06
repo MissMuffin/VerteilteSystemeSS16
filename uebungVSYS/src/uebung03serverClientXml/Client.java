e92c0c30c78ad5184408bf0cf410b8f21152711b
die Sabi <nigistsaba@hotmail.com>
2016-06-06 17:01:26 +0200

    client example from mr. haase

--- uebungVSYS/src/uebung03serverClientXml/Client.java
+++ uebungVSYS/src/uebung03serverClientXml/Client.java
@@ -1,5 +1,32 @@
 package uebung03serverClientXml;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
+	
+	public static void main(String args[]) {
+		try{
+			int serverPort = 7896;
+			Socket s = new Socket(args[1], serverPort);
+			DataInputStream in = new DataInputStream(s.getInputStream());
+			DataOutputStream out = new DataOutputStream(s.getOutputStream());
+			out.writeUTF(args[0]);
+			String data = in.readUTF();
+			System.out.println("Received: " + data);
+			s.close();
+		}catch(UnknownHostException e){
+			System.out.println("Sock: " + e.getMessage());
+		}catch(EOFException e){
+			System.out.println("EOF: " + e.getMessage());
+		}catch(IOException e){
+			System.out.println("IO: " + e.getMessage());
+		}
+		
+	}
 		
 }
